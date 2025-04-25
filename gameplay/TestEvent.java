599132eac63060c516a9fd6d3131017e7864490e
junjinyun <kjkj4142@naver.com>
2025-04-25 21:38:13 +0900

    feat : 각 코드들의 디버깅 및 추후 사용을 위한 코드 예제

---
+++ gameplay/TestEvent.java
@@ -0,0 +1,15 @@
+package gameplay;
+
+import gameplay.Party.AllyParty;
+import gameplay.Event.RandomEventGenerator;
+
+public class TestEvent {
+
+    public static void main(String[] args) {
+        // AllyParty 객체 생성
+        AllyParty allyParty = new AllyParty();
+        
+        // RandomEventGenerator에서 해당 파티 객체를 사용하도록 수정
+        RandomEventGenerator.EventGenerator(allyParty);
+    }
+}
