0c460d8320212bd549e598af9941e1c35122cef2
junjinyun <kjkj4142@naver.com>
2025-05-07 10:11:33 +0900

    feat : 스테이지 정보 로드하는 클래스 생성

--- loaddata/Stage.java
+++ loaddata/Stage.java
@@ -1,18 +1,45 @@
 package loaddata;
 
-/* 스테이지 클래스 */
-
 public class Stage {
-    private int Stage;
-    private int Cost;
+    private int id;
+    private String stageName;
+    private String enemyType;
+    private String bgImagesource;
+    private int powerGrowth;
+    private String bosstype;
 
     public Stage() {}
 
-    public int getStage() {
-        return Stage;
+    public Stage(int id, String stageName, String enemyType, String bgImagesource, int powerGrowth, String bosstype) {
+        this.id = id;
+        this.stageName = stageName;
+        this.enemyType = enemyType;
+        this.bgImagesource = bgImagesource;
+        this.powerGrowth = powerGrowth;
+        this.bosstype = bosstype;
     }
 
-    public int getCost() {
-        return Cost;
+    public int getId() {
+        return id;
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public String getEnemyType() {
+        return enemyType;
+    }
+
+    public String getBgImagesource() {
+        return bgImagesource;
+    }
+
+    public int getPowerGrowth() {
+        return powerGrowth;
+    }
+
+    public String getBosstype() {
+        return bosstype;
     }
 }
